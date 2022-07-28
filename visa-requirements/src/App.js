a665ac0046067554c19b235299e279187d75ec8f
Charaf Mrah <charaf4charaf@gmail.com>
2022-07-28 12:12:06 +0300

    Bootstrap

--- visa-requirements/src/App.js
+++ visa-requirements/src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    
-
     <div className="App">
-      <header className="App-header">
-
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
